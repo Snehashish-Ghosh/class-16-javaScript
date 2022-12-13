54fc89fa292f748316d93fcfeb35262cd7ae9909
Snehashish-Ghosh <necshuvo@gmail.com>
2022-12-13 18:39:16 +0600

    javaScript

---
+++ part 1/script.js
@@ -0,0 +1,26 @@
+// First part
+
+let life = "beautifull";
+if (life === "beautifull") alert("Life is beautifull");
+
+console.log(3 + 3 - 4);
+console.log(0 * 1 + 1);
+
+// 1St variable
+let firstName = "Shuvo";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let lastName = "nec";
+
+console.log(lastName);
+console.log(lastName);
+console.log(lastName);
+
+let number = 9;
+
+console.log(number);
+console.log(number);
+console.log(number);
